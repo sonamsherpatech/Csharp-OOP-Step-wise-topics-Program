cf8656846590c312de5cd09b60121a6eb602fd6c
sonamsherpatech <sonam.sherpa.tech@gmail.com>
2025-06-20 11:25:44 +0545

    add abstract class and method file and updated notes

---
+++ AbstractClassAndMethods/AbstractClassAndMethods/AbstractEx2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+
+namespace AbstractClassAndMethods
+{
+    public abstract class Employee
+    {
+        public int id;
+        public string name;
+
+        public Employee(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public abstract double CalculateSalary();
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name:{name}\nID:{id}\nSalary:{CalculateSalary()}");
+        }
+    }
+
+    public class FullTimeEmployee : Employee
+    {
+        public double monthlySalary;
+        public FullTimeEmployee(int id, string name,double monthlySalary):base(id,name)
+        {
+            this.monthlySalary = monthlySalary;
+        }
+
+        public override double CalculateSalary()
+        {
+            return monthlySalary;
+        }
+    }
+
+    public class PartTimeEmployee : Employee
+    {
+        public double hourlyRate, hoursWorked;
+        public PartTimeEmployee(int id, string name, double hourlyRate, double hoursWorked) : base(id,name)
+        {
+            this.hourlyRate = hourlyRate;
+            this.hoursWorked = hoursWorked;
+        }
+
+        public override double CalculateSalary()
+        {
+            return hourlyRate * hoursWorked;
+        }
+    }
+
+    class AbstractEx2
+    {
+        public static void Main()
+        {
+            FullTimeEmployee f1 = new FullTimeEmployee(4500, "Sonam Sherpa", 4500.63);
+            f1.DisplayInfo();
+
+            PartTimeEmployee p1 = new PartTimeEmployee(6500, "Rohan Shrestha", 12.50, 6);
+            p1.DisplayInfo();
+        }
+    }
+}
---
+++ AbstractClassAndMethods/AbstractClassAndMethods/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AbstractClassAndMethods
+{
+    public abstract class Shape {
+        public double dim1, dim2;
+
+        public Shape(double dim1, double dim2)
+        {
+            this.dim1 = dim1;
+            this.dim2 = dim2;
+        }
+
+        public abstract double FindArea();
+    }
+
+    public class Triangle : Shape
+    {
+        public Triangle(double dim1, double dim2) : base(dim1, dim2)
+        {
+        }
+
+        public override double FindArea()
+        {
+            return 0.5 * base.dim1 * base.dim2;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle t1 = new Triangle(4, 5);
+            Console.WriteLine($"Area of Triangle is {t1.FindArea()}");
+        }
+    }
+}
