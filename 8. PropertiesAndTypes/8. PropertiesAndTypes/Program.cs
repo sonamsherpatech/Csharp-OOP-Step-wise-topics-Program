e8b67db4a6dc6659ae5c98cdf62bf94c78f103a7
sonamsherpatech <sonam.sherpa.tech@gmail.com>
2025-06-21 11:26:22 +0545

    add concept of property and its types with two programming example

--- 8. PropertiesAndTypes/8. PropertiesAndTypes/Program.cs
+++ 8. PropertiesAndTypes/8. PropertiesAndTypes/Program.cs
@@ -1,15 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _8.PropertiesAndTypes
 {
+    public class Employee {
+        //Auto-Implemented Property
+        public int EID { get; set; }
+
+        private string name;
+        private double salary;
+        //Read-Only
+        public string NameR
+        {
+            get { return name; }
+        }
+
+        //Write-Only
+        public string NameW
+        {
+            set { name = value; }
+        }
+
+
+        //Read-Write
+        public double Salary
+        {
+            set{ salary = value; }
+            get { return salary;  }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Employee e1 = new Employee();
+            e1.EID = 101;
+            e1.NameW = "Sonam Sherpa";
+            e1.Salary = 4500.65;
+
+            Console.WriteLine("...Employee Details...");
+            Console.WriteLine($"{e1.EID}\t{e1.NameR}\t{e1.Salary}");
         }
     }
 }
---
+++ 8. PropertiesAndTypes/8. PropertiesAndTypes/PropertyEx2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _8.PropertiesAndTypes
+{
+    class UserProfile
+    {
+        //Auto-Implemented property
+        public string UserName { get; set; }
+        private string email;
+
+        //Read-Only Property
+        public string Email
+        {
+            get { return email; }
+        }
+        //using constructor to write the email property
+        public UserProfile(string email)
+        {
+            this.email = email;
+        }
+
+        private string password;
+        //Write-Only Property
+        public string Password
+        {
+            set { password = value; }
+        }
+
+        private int age;
+        //Read-Write Property
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+    }
+
+    class PropertyEx2
+    {
+        public static void Main()
+        {
+            UserProfile p1 = new UserProfile("hello@gmail.com");
+            p1.UserName = "xhello";
+            p1.Password = "hello123";
+            p1.Age = 22;
+
+            Console.WriteLine("User Profile");
+            Console.WriteLine($"{p1.UserName}\t{p1.Email}\t{p1.Age}\nEdit");
+        }
+    }
+}
